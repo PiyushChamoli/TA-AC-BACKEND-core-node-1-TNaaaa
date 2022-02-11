6b145c3463b8a62c514920e5071852149b36971f
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-02-12 00:26:48 +0530

    update index.js

---
+++ block-BNaaar/index.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req,res) {
+    let parsedURL = url.parse(req.url);
+    let pathName = parsedURL.pathname;
+    console.log(req.method, pathName);
+    if ((req.method === 'GET') && (req.url === pathName)) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('Welcome to homepage');
+        res.end();
+    }else if ((req.method === 'GET') && (req.url === pathName)){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h2>This is all about NodeJS</h2>');
+        res.end();
+    }else if ((req.method === 'POST') && (req.url === pathName)) {
+        res.writeHead(201,{'Content-Type':'text/json'});
+        res.end(`{message: this is a post request}`);
+    }
+}
+
+server.listen(5000, () => {
+    console.log('Listening on server 5000')
+})
