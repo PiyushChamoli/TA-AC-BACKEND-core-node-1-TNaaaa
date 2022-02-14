2329ded2f7025481e3a27e2d188512a6ead4fdc9
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-02-14 20:24:03 +0530

    update index.js

--- block-BNaaav/index.js
+++ block-BNaaav/index.js
@@ -4,12 +4,37 @@ var fs = require('fs');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req,res) {
+    console.log(req.url, req.method);
     if (req.method === 'GET' && req.url === '/') {
         res.setHeader('Content-Type','text/html');
         fs.createReadStream('./index.html').pipe(res);
-    } 
+    }
+    if(req.url.split('.').pop() === 'css') {
+        res.setHeader('Content-Type', 'text/css');
+        fs.readFile('.' + req.url, (err, content) => {
+          if(err) return console.log(err);
+          res.end(content)
+        })
+    }
+    if(req.url.split('.').pop() === 'jpg') {
+        res.setHeader('Content-Type', 'image/jpg');
+        fs.readFile('.' + req.url, (err, content) => {
+          if(err) return console.log(err);
+          res.end(content)
+        })
+    }
+    if(req.url.split('.').pop() === 'js') {
+        res.setHeader('Content-Type', 'application/javascript');
+        fs.readFile('.' + req.url, (err, content) => {
+          if(err) return console.log(err);
+          res.end(content)
+        })
+    }
+    
 }
 
 server.listen(5000, () => {
     console.log('server listening on port 5000')
 })
+
+
